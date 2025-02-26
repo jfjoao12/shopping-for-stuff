7846e3cec9ff664e01483fb4ed1cac3634ccd25f
Wilson Reid <wilsonthehungryman@gmail.com>
2025-02-26 15:06:59 -0600

    Added routes for the checkout controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :products, only: [ :index, :show ]
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
   root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
